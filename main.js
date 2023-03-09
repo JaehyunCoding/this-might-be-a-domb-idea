5a0f528918e54b3f9a9307162c7a65ece0f3a11f
JaehyunCoding <kq1945@gmail.com>
2023-03-09 15:26:56 -0500

    Good

---
+++ main.js
@@ -0,0 +1,26 @@
+let para = document.querySelector("p");
+para.style.color = "lightblue"
+
+let header = document.querySelector("h1");
+header.style.fontSize = "48px";
+
+para.innerText = "Lorem ipsum dolor amet viral meh selfies drinking vinegar, intelligentsia poke flannel twee paleo enamel pin cray. Banjo celiac crucifix, kickstarter la croix air plant jianbing hashtag vinyl hell of man bun selvage schlitz banh mi. Tacos hella raclette quinoa blog, williamsburg adaptogen tbh. Hexagon af stumptown lumbersexual synth gentrify quinoa enamel pin celiac master cleanse. Truffaut typewriter shoreditch, semiotics iceland mixtape taxidermy umami distillery austin hashtag. Food truck synth wayfarers, street art banh mi actually authentic. Bitters tousled tattooed vegan neutra pug hell of fixie chia unicorn letterpress."
+
+let item13 = document.querySelector("#item-13")
+item13.style.opacity = ".5"
+
+let item3 = document.querySelector("#item-3")
+item3.innerText = `I say, "Hi!"`
+
+let myImg = document.querySelector("img")
+myImg.src = "https://pbs.twimg.com/media/FqDF1UlakAEo4uD?format=jpg&name=900x900"
+myImg.style.height = "300px"
+
+let myImg2 = document.querySelector("#img2")
+// # hashtag is for id
+myImg2.style.height = "300px"
+
+let item16 = document.querySelector("#item-16")
+item16.className = "item"
+item16.innerText = "Won't get fooled again."
+
